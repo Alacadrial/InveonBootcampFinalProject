89b2ebc1777c0693946014b9923435ac8ae0d38b
Alacadrial <alacadrial@gmail.com>
2023-11-23 20:46:44 +0300

    User login and acquiring token, slice modification

--- Frontend/src/app/slices/user.js
+++ Frontend/src/app/slices/user.js
@@ -1,22 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { authenticateUser } from "../../utils/auth.js";
+import { decodeJwt } from "../../utils/utils.js";
+import { useDispatch } from "react-redux";
+import { getCartByUserId } from "./product.js";
 
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        status: true,
-        user: {}
+        status: false,
+        loadStatus: "idle",
+        error: null,
+        user: {},
     },
     reducers: {
-        login: (state) => {
-            state.status = true
-            //rest api den gelen veriye göre değiştirebilir. 
-            state.user = {
-                name: 'ibrahim',
-                role: 'customer',
-                email: 'ibr@ibr.com',
-                pass: '123456'
-            }
-        },
         register: (state, action) => {
             let { name, email, pass } = action.payload;
             state.status = true
@@ -28,14 +24,48 @@ const userSlice = createSlice({
             }
         },
         logout: (state) => {
-            console.log("user slice logouta gelindi");
             state.status = false
             state.user = {
-
             }
         }
-    }
+    },
+    extraReducers: (builder) => {
+        // Handle the fulfilled and rejected actions from getProductByIdAsync
+        builder
+          .addCase(loginAsync.pending, (state) => {
+            state.loadStatus = 'loading';
+          })
+          .addCase(loginAsync.fulfilled, (state, action) => {
+            let {username, pwd, token} = action.payload; 
+            let decodedObj = decodeJwt(token);
+            state.loadStatus = 'succeeded';
+            state.status = true;
+            state.user = {
+                name: decodedObj.given_name + " " + decodedObj.family_name,
+                userId: decodedObj.sub,
+                role: 'customer',
+                email: username,
+                pass: pwd,
+                token: token
+            };
+          })
+          .addCase(loginAsync.rejected, (state, action) => {
+            state.loadStatus = 'failed';
+            state.error = action.error.message;
+          });
+      },
 })
 
+// Create an asynchronous thunk
+export const loginAsync = createAsyncThunk(
+    'user/loginAsync',
+    async (credentials) => {
+        let {username, pwd} = credentials
+        let token = await authenticateUser(username, pwd);
+        return {username, pwd, token};
+    }
+);
+
 const userReducer = userSlice.reducer
+export const { register, logout } = userSlice.actions;
 export default userReducer
--- Frontend/src/components/Login/index.js
+++ Frontend/src/components/Login/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from "react-redux";
 import Swal from 'sweetalert2';
 import { useNavigate } from "react-router-dom"
+import { loginAsync } from '../../app/slices/user';
+import { getCartByUserId } from '../../app/slices/product';
+
 
 const LoginArea = () => {
     let dispatch = useDispatch();
@@ -11,8 +14,20 @@ const LoginArea = () => {
     let status = useSelector((state) => state.user.status);
     let user = useSelector((state) => state.user.user);
 
+    useEffect(()=>{
+        if(status){
+            dispatch(getCartByUserId(user.userId));
+            Swal.fire({
+                icon: 'success',
+                title: 'Giriş Başarılı',
+                text: 'Hoşgeldiniz '+ user.name
+            });
+            history("/");
+        }
+    }, [user]);
+
     // Login
-    const login = () => {
+    const login = (uname, password) => {
         if(status){
             Swal.fire({
                 icon: 'question',
@@ -30,18 +45,8 @@ const LoginArea = () => {
                 }
               });
         }else{
-            dispatch({ type: "user/login" })
-            let name = user.name || 'Customer'
-            console.log(typeof(user.name));
-            Swal.fire({
-                icon: 'success',
-                title: 'Giriş Başarılı',
-                text: 'Hoşgeldiniz '+ name
-            })
-            history("/");
+            dispatch(loginAsync({username: uname, pwd: password}))
         }
-        
-
     }
 
     return (
@@ -52,14 +57,14 @@ const LoginArea = () => {
                         <div className="col-lg-6 offset-lg-3 col-md-12 col-sm-12 col-12">
                             <div className="account_form">
                                 <h3>Giriş Yap</h3>
-                                <form onSubmit={(e)=>{e.preventDefault();login()}}>
+                                <form onSubmit={(e)=>{e.preventDefault();login(e.target.uname_input.value, e.target.passwd_input.value);}}>
                                     <div className="default-form-box">
                                         <label>Kullanıcı adı veya Email<span className="text-danger">*</span></label>
-                                        <input type="text" className="form-control" required defaultValue="ibr@ibr.com"/>
+                                        <input type="text" id="uname_input" className="form-control" required defaultValue="customer1@gmail.com"/>
                                     </div>
                                     <div className="default-form-box">
                                         <label>Şifre<span className="text-danger">*</span></label>
-                                        <input type="password" className="form-control" required defaultValue="123456" minLength="8"/>
+                                        <input type="password" id="passwd_input" className="form-control" required defaultValue="Admin123*" minLength="8"/>
                                     </div>
                                     <div className="login_submit">
                                         <button className="theme-btn-one btn-black-overlay btn_md" type="submit">Giriş Yap</button>
